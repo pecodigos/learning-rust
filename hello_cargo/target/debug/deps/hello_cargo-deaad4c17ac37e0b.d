1ae7405ace3337dd501268c187f5e8bdfadbb726
pecodigos <pedrenriquecunha@gmail.com>
2024-07-21 04:36:16 -0300

    First Hello World with Rust + Cargogit add .!

---
+++ hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b.d
@@ -0,0 +1,5 @@
+/home/pedrenrique/Documents/Rust/hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b: src/main.rs
+
+/home/pedrenrique/Documents/Rust/hello_cargo/target/debug/deps/hello_cargo-deaad4c17ac37e0b.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/pedrenrique/Documents/Rust/hello_cargo/target/debug/hello_cargo: /home/pedrenrique/Documents/Rust/hello_cargo/src/main.rs
