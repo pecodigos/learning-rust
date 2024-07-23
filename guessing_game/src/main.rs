f47323046696868addddeb7c6e36dc85ac499eb9
pecodigos <pedrenriquecunha@gmail.com>
2024-07-23 04:37:32 -0300

    secret number game in Rust!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,36 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Guess the number!");
-    println!("Please, input your guess: ");
+    println!("Guess the secret number!");
 
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng().gen_range(1..=10);
 
-    io::stdin()
+    loop {
+        println!("Please, input your guess: ");
+
+        let mut guess = String::new();
+        
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {}\n", guess);
 
-    println!("\nYou guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You got it right!");
+                break;
+            }
+        }
+    }
+
 }
